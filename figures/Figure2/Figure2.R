ccffbbd1202357f2906ed16127fc47ad5c3531a3
caleblareau <caleblareau@g.harvard.edu>
2017-08-26 02:42:24 -0400

    fixed rna

---
+++ figures/Figure2/Figure2.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(BuenColors)
+
+cellCoordsDF <- data.frame(
+  CellLabel = c("HSC", "MPP", "LMPP", "CLP", "GMP1", "GMP2", "GMP3", "CMP", "MEP", "NK", "CD4", "CD8", "B", "pDC", "Mono", "Ery")
+          x = c(), 
+          y = c(10       8,      7,     5,     6,        5,     5,     7,     5,    2,     2,     2,     2, )
+)
