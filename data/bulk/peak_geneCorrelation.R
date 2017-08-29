b2e5d1fef3147285fd5bf2633fde43b55b940617
caleblareau <caleblareau@g.harvard.edu>
2017-08-29 17:52:22 -0400

    new peaks w 500bp; updated correlations

--- data/bulk/peak_geneCorrelation.R
+++ data/bulk/peak_geneCorrelation.R
@@ -8,26 +8,27 @@ library(diffloop)
 # ---------------------------------------------
 
 # Import genomic coordinates
-peaks_g <- rmchr(makeGRangesFromDataFrame(fread("ATAC/26August2017_EJCsamples_allReads_250bp.bed"),
-                                    seqnames.field = "V1", start.field = "V2", end.field = "V3"))
-tss_g <- makeGRangesFromDataFrame(data.frame(fread("RNA/geneTSScoordinates.txt")), keep.extra.columns = TRUE,
+peaks_g <- makeGRangesFromDataFrame(fread("ATAC/29August2017_EJCsamples_allReads_500bp.bed"),
+                                    seqnames.field = "V1", start.field = "V2", end.field = "V3")
+tss_g <- makeGRangesFromDataFrame(data.frame(fread("RNA/refGene_hg19_TSS.bed")), keep.extra.columns = TRUE,
                                   seqnames.field = "V1", start.field = "V2", end.field = "V2")
 
 # Import other raw data
 rna <- fread("RNA/16populations_RNAcounts.txt")
 genes <- rna[["Genes"]]
 rna <- data.matrix(rna[,Genes:=NULL])
-atac <- data.matrix(fread("ATAC/26August2017_EJCsamples_allReads_250bp.counts.txt"))[,colnames(rna)]
+atac <- data.matrix(fread("ATAC/29August2017_EJCsamples_allReads_500bp.counts.txt"))[,colnames(rna)]
 
 # Make it counts per million normalized
 rna <- sweep(rna, 2, colSums(rna), FUN="/") * 1000000
 atac <- sweep(atac, 2, colSums(atac), FUN="/") * 1000000
 
 # And filter genes everywhere
-keepGenes <- which(rowMeans(rna) > 1)
+keepGenes <- which(rowMeans(rna) > 1 & !is.na(match(genes, mcols(tss_g)$V5)))
+length(keepGenes)
 genes <- genes[keepGenes]
 rna <- rna[keepGenes, ]
-tss_g <- padGRanges(tss_g[mcols(tss_g)[,1] %in% genes], pad = 1000000)
+tss_g <- sort(padGRanges(tss_g[mcols(tss_g)[,3] %in% genes], pad = 1000000))
 
 # Define a function to get pvalue from correlation given a sample size
 rhoToP <- function(rho, n = 16){
@@ -37,7 +38,7 @@ rhoToP <- function(rho, n = 16){
 
 # Loop over genes to compute correlation, pvalue, etc. 
 lappout <- lapply(genes, function(gene){
-  atacidx <- queryHits(findOverlaps(peaks_g, tss_g[which(mcols(tss_g)[,1] == gene)]))
+  atacidx <- unique(queryHits(findOverlaps(peaks_g, tss_g[which(mcols(tss_g)[,3] == gene)])))
   if(length(atacidx) > 1){
     rho <- cor(log2(rna[which(genes == gene)[1],]+1), t(log2(atac[atacidx,]+1)))[1,]
     return(data.frame(data.frame(peaks_g[atacidx])[,c(1,2,3)], gene = gene, rho = rho, p = rhoToP(rho)))
@@ -48,33 +49,8 @@ lappout <- lapply(genes, function(gene){
 
 # Make final output
 df <- rbindlist(Filter(Negate(is.null), setNames(lappout,seq_along(lappout))))
+df$rho <- round(df$rho, digits = 3)
+df$p <- formatC(df$p, format = "e", digits = 2)
 write.table(df, file = "peakGeneCorrelation.tsv", 
             row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
-
-
-# Make TSS Data Frame
-if(FALSE){
-  
-  # Determine TSS per gene
-  gtf <-rtracklayer::import("RNA/hg19_10X.gtf.gz")
-  gs <- tapply(start(gtf), list(mcols(gtf)@listData$gene_id), min)
-  ge <- tapply(start(gtf), list(mcols(gtf)@listData$gene_id), max)
-  chrs <- data.frame(chr = seqnames(gtf), gene = mcols(gtf)@listData$gene_id) %>% unique()
-  strand <- data.frame(strand =strand(gtf), gene = mcols(gtf)@listData$gene_id) %>% unique()
-  remove(gtf)
-  coords <- data.frame(chrs, gs, ge, strand); coords$gene <- rownames(coords)
-  
-  # Import Common Gene Coordinates 
-  cgc <- read.table("RNA/genes.tsv")
-  stopifnot(all(coords$gene.1 == cgc$V1))
-  
-  coords$commonName <- cgc[,2]
-  
-  tssdf <- rbind(setNames(coords[coords$strand == "+", c(1,3,7)], c("chr", "start", "gene")), 
-                 setNames(coords[coords$strand == "-", c(1,4,7)], c("chr", "start", "gene")))
-  rownames(tssdf) <- NULL
-  write.table(tssdf, file = "RNA/geneTSScoordinates.txt",
-              row.names = FALSE, col.names = FALSE, sep = "\t", quote = FALSE)
-}
-
 
