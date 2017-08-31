ee8b324885dbca4722552f39338c03a5466e381b
caleblareau <caleblareau@g.harvard.edu>
2017-08-31 03:20:30 -0400

    moved finemap stuff

--- figures/Figure3/01_runWchromVAR.R
+++ figures/Figure3/01_runWchromVAR.R
@@ -7,7 +7,17 @@ library(BSgenome.Hsapiens.UCSC.hg19)
 source("00_smooth.R")
 
 SE <- readRDS("../../data/singlecell/scATAC/full_scHeme_QC.rds")
+pcaPlus <- data.table::fread("../../data/singlecell/scATAC/scHeme_meta.txt")
 SE <- addGCBias(SE, genome = BSgenome.Hsapiens.UCSC.hg19)
 ukbb <- importBedScore(rowRanges(SE), list.files("../../data/UKBB_BC_PP001/", full.names = TRUE))
 ukbb_wDEV <- computeWeightedDeviations(SE, ukbb)
 
+zscoreRaw <- t(assays(ukbbDEV)[["z"]])
+colnames(zscoreRaw) <- paste0("raw_", colnames(zscoreRaw))
+smoothCellxCell <- smoothPCAmat(pcaPlus[,-c(1,2)])
+zscoreSmooth <- (smoothCellxCell %*% zscoreRaw)
+colnames(zscoreSmooth) <- paste0("smooth_", colnames(zscoreSmooth))
+
+bigdf <- data.frame(celltypes, pca, data.matrix(zscoreRaw), data.matrix(zscoreSmooth))
+write.table(bigdf, file = "", row.names = FALSE, col.names = TRUE,
+            quote = FALSE, sep = "\t")
