327973b53b2d60535f859a3fda7ab6a0a03b3a56
caleblareau <caleblareau@g.harvard.edu>
2017-09-26 23:14:13 -0400

    relative enrichment analysis

---
+++ figures/Figure2/04_lineageSpecific.R
@@ -0,0 +1,70 @@
+library(BuenColors)
+library(ggplot2)
+
+set.seed(14651)
+
+
+# Define lineage specific attributes
+Ery <- c("HSC", "MPP", "CMP", "MEP", "Ery")
+Meg <- c("HSC", "MPP", "CMP", "MEP", "Mega")
+Mye <- c("HSC", "MPP", "CMP", "LMPP", "GMP-A", "GMP-B", "GMP-C", "Mono", "mDC")
+Lymph <- c("HSC", "MPP", "LMPP", "CLP", "NK", "pDC", "CD4", "CD8", "B")
+
+make2df <- function(trait, celltypes){
+  return(data.frame(Celltype = celltypes, Trait = rep(trait, length(celltypes))))
+}
+
+lineageSpecificDF <- rbind(
+  make2df("BASO_COUNT", Mye),
+  make2df("EO_COUNT", Mye),
+  make2df("NEUTRO_COUNT", Mye),
+  make2df("MONO_COUNT", Mye),
+  make2df("WBC_COUNT", unique(c(Mye, Lymph))),
+  make2df("LYMPH_COUNT", Lymph),
+  make2df("PLT_COUNT", Meg),
+  make2df("MPV", Meg),
+  
+  make2df("MEAN_RETIC_VOL", Ery),
+  make2df("HCT", Ery),
+  make2df("HGB", Ery),
+  make2df("MCH", Ery),
+  make2df("MCV", Ery),
+  make2df("MCHC", Ery),
+  make2df("RBC_COUNT", Ery),
+  make2df("RETIC_COUNT", Ery)
+)
+
+# Return T/F whether rows in df1 are in df2
+rowCheck <- function(df1, df2){
+  xx <- apply(df1, 1, paste, collapse = "_")
+  yy <- apply(df2, 1, paste, collapse = "_")
+  return(xx %in% yy)
+}
+
+df <- read.table("../../data/bulk/GWAS-Bulk/compare3.tsv", header = TRUE)               
+df$weighted_pvalue <- pnorm(df$weighted_Zscore, lower.tail = FALSE)
+df$chromVAR_pvalue <- pnorm(df$chromVAR_Zscore, lower.tail = FALSE)
+
+df$hit_ldscore <- df$ldscore_pvalue < 0.05/288
+df$hit_chromVAR <- df$chromVAR_pvalue < 0.05/288
+df$hit_weighted <- df$weighted_pvalue < 0.05/288
+
+df$lineageSpecific <- rowCheck(df[,c("Celltype", "Trait")], lineageSpecificDF)
+odf <- df[,c(1,2,5:11)] 
+#write.table(odf, file = "../../supplemental_tables/Bulk_Enrichments.tsv", 
+#            quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+
+permuted <- sapply(1:10000, function(i) sum(1:dim(df)[1] * sample(df$lineageSpecific, length(df$lineageSpecific))))
+ldscore_ranksum <- sum(1:dim(df)[1]*df[order(df$ldscore_pvalue, decreasing = FALSE), "lineageSpecific"])
+chromVAR_ranksum <- sum(1:dim(df)[1]*df[order(df$chromVAR_pvalue, decreasing = FALSE), "lineageSpecific"])
+weighted_ranksum <- sum(1:dim(df)[1]*df[order(df$weighted_pvalue, decreasing = FALSE), "lineageSpecific"])
+
+pnorm((mean(permuted) - ldscore_ranksum)/sd(permuted), lower.tail = FALSE)
+pnorm((mean(permuted) - chromVAR_ranksum)/sd(permuted), lower.tail = FALSE)
+pnorm((mean(permuted) - weighted_ranksum)/sd(permuted), lower.tail = FALSE)
+
+tfdf <- data.frame(
+  truePos = c(sum(df$lineageSpecific& df$hit_ldscore), sum(df$lineageSpecific& df$hit_chromVAR), sum(df$lineageSpecific& df$hit_weighted)),
+  falsePos = c(sum(!df$lineageSpecific& df$hit_ldscore), sum(!df$lineageSpecific& df$hit_chromVAR), sum(!df$lineageSpecific& df$hit_weighted))
+)
+rownames(tfdf) <- c("LDscore", "chromVAR", "g-chromVAR")
