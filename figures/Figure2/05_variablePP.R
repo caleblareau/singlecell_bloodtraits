7a6ba52a822259d44ebd1e514412a5a6dcef9f10
caleblareau <caleblareau@g.harvard.edu>
2017-09-28 09:55:32 -0400

    more sc stuff

--- figures/Figure2/05_variablePP.R
+++ figures/Figure2/05_variablePP.R
@@ -7,6 +7,7 @@ library(BSgenome.Hsapiens.UCSC.hg19)
 library(reshape2)
 library(diffloop)
 library(Matrix)
+ library(BuenColors)
 
 # Create bulk Summarized Experiment
 peaksdf <- fread("../../data/bulk/ATAC/29August2017_EJCsamples_allReads_500bp.bed")
@@ -17,24 +18,22 @@ SE <- SummarizedExperiment(assays = list(counts = counts),
                                colData = DataFrame(names = colnames(counts)))
 SE <- addGCBias(SE, genome = BSgenome.Hsapiens.UCSC.hg19)
 ukbb <- importBedScore(rowRanges(SE), list.files("../../data/UKBB_BC_PP001/", full.names = TRUE, pattern = ".bed"))
+bgp <- getBackgroundPeaks(SE)
 
 # Make variable PP thresholds
 makePPthreshdf <- function(val){
+  print(val)
   ukbbcp <- ukbb
   assays(ukbbcp)$weights[assays(ukbbcp)$weights < val] <- 0
-  zw<- melt(t(assays( computeWeightedDeviations(SE, ukbbcp))[["z"]]))
+  zw<- melt(t(assays( computeWeightedDeviations(SE, ukbbcp, background_peaks = bgp))[["z"]]))
   zw$PP <- val
   return(zw)
 }
 
-PPs <- c(1:10/1000, 2:100/100)
+PPs <- c(1:10/1000, 2:9/10, 1)
+listres <- lapply(PPs, makePPthreshdf)
 
-zscoreWeighted <- rbindlist(lapply(head(PPs,2), makePPthreshdf))
+zscoreWeighted <- rbindlist(listres)
+saveRDS(zscoreWeighted, "../../data/supplement_rds/varyingPPcutoff.rds")
 
-ggplot(zscoreWeighted, aes(x = PP, y = -log10(pnorm(value,lower.tail = FALSE)))) +
-  geom_line(aes(color = Var1)) +
-  pretty_plot() + labs(x = "Posterior Probability", y = "g-chromVAR Enrichment (-log10 p)", color = "") +
-   scale_color_manual(values = ejc_color_maps) + facet_wrap(~Var2, scales = "free_y") +
-  theme(legend.position="bottom") 
-#  geom_hline(yintercept = -log10(0.05 / (16*17)), linetype = 2) +
 
