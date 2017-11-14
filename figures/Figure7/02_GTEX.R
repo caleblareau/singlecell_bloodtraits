641bfab0ae760ee62e1acf57af11d5707386432b
caleblareau <caleblareau@g.harvard.edu>
2017-11-13 19:27:53 -0500

    misc. caleb plots

---
+++ figures/Figure7/02_GTEX.R
@@ -0,0 +1,54 @@
+library(data.table)
+library(dplyr)
+library(annotables)
+library(dplyr)
+
+gtex <- fread(paste0('zcat < ', "../../data/Whole_Blood.v7.signif_variant_gene_pairs.txt.gz"))
+pj <- stringr::str_split_fixed(gtex$variant_id, "_", n = 5)
+gtex$chr <- paste0("chr", pj[,1])
+gtex$bp <- as.numeric(pj[,2])
+gtex$ref <- pj[,3]
+gtex$alt <- pj[,4]
+
+lapply(list.files("../../data/UKBB_BC_PP001/", pattern = "*.bed$", full.names = TRUE), function(f){
+  dt <- fread(f)
+  dt$trait <- f
+  dt
+}) %>% rbindlist() %>% as.data.frame() -> allVars
+
+
+allVars$trait <- gsub("_PP001.bed", "", gsub("../../data/UKBB_BC_PP001//", "", allVars$trait))
+filt <- allVars[allVars$V5 > 0.5,]
+
+mdf <- merge(allVars, gtex, by.x = c("V1", "V2"), by.y = c("chr", "bp"))
+cleandf <- mdf[,c("V1", "V2", "trait", "V5", "gene_id", "tss_distance", "pval_nominal", "ref", "alt", "slope")]
+var <- stringr::str_split_fixed(cleandf$gene_id, "[.]", n = 2)
+cleandf$gene_id2 <- var[,1]
+cleandf2 <- merge(cleandf, grch37, by.x = "gene_id2", by.y = "ensgene", all.x = TRUE)
+df <- cleandf2[,c("V1", "V2", "trait", "V5", "gene_id", "tss_distance",
+                  "pval_nominal", "symbol", "description", "ref", "alt", "slope")]
+saveRDS(df, file = "bloodEQTLS_gtex.rds")
+
+df <- readRDS("bloodEQTLS_gtex.rds")
+
+dff <- df[df$V1 != "chr17" & df$V1 != "chr6" & df$V5 > 0.5, ]
+sort(table(unique(dff[,c("V2", "symbol", "trait")])[,c(1)]), decreasing = TRUE) %>% head(40)
+dff[dff$V2 == "34195572", ]
+
+
+ss <- (df[df$V5 > 0.05 ,c("V2", "symbol", "tss_distance")])
+a <- ss %>% group_by(symbol) %>% summarise(mint = min(tss_distance), maxt = max(tss_distance))
+a$diff <- a$maxt - a$mint
+head(a[order(a$diff, decreasing = TRUE),], 20)
+
+head(sort(table(unique(ss)$symbol), decreasing = TRUE), 80)
+
+df[abs(df$tss_distance) > 100000 & df$V5 > 0.5 & df[,1] != "chr17",1:8 ]
+
+
+LD <- readRDS("../../data/Finemap/LD80.rds")
+plt <- LD[["PLT_COUNT"]]
+plt[(plt$position1 %in% df[df$V5 > 0.05 & df$symbol == "RHD","V2"]) | (plt$position2 %in% df[df$V5 > 0.05 & df$symbol == "RHD","V2"]), ]
+
+rbc <- LD[["RBC_COUNT"]]
+rbc[(rbc$position1 %in% df[df$V5 > 0.1 & df$symbol == "ABO","V2"]) | (rbc$position2 %in% df[df$V5 > 0.05 & df$symbol == "ABO","V2"]), ]
---
+++ figures/Figure7/03_relativeExpression.R
@@ -0,0 +1,28 @@
+library(data.table)
+library(BuenColors)
+
+gene <- "IL11RA"
+
+rna <- data.frame(fread(paste0("../../data/bulk/RNA/16populations_RNAcounts.txt")))
+genes <- rna[,1]
+counts <- data.matrix(rna[,-1])
+cpm <- sweep(counts, 2, Matrix::colSums(counts), FUN="/") * 1000000
+colnames(cpm) <- c("HSC", "MPP", "LMPP", "CLP", "GMP-A", "GMP-B", "GMP-C", "CMP", "MEP", "NK", "CD4", "CD8", "B", "pDC", "Mono", "Ery")
+
+# Pull out gene and 
+ix <- which(genes == gene)
+plotdf <- data.frame(Name = colnames(cpm),
+                     Expression = cpm[ix,], 
+                     Expm = (cpm[ix,]/sum(cpm[ix,])) * 100)
+plotdf <- plotdf[order(plotdf$Expm, decreasing = FALSE), ]
+plotdf$pos <- 0.5 * (cumsum(plotdf$Expm) + cumsum(c(0, plotdf$Expm[-length(plotdf$Expm)])))
+p <- ggplot(plotdf, aes(x=pos, y=Expression, width=Expm)) + 
+  geom_bar(aes(fill=Name), stat="identity",  color = "black") +
+  scale_fill_manual(values = ejc_color_maps) + pretty_plot() + coord_flip() +
+  theme(legend.position = "none") + ylab(paste0(gene, " Expression (TPM)")) +
+  xlab("") + theme(panel.grid = element_blank(), panel.border = element_blank()) +
+  theme(axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank())
+  
+cowplot::ggsave(filename = paste0("plots/Expression_", gene, ".pdf"), p, width = 5, height = 5)
