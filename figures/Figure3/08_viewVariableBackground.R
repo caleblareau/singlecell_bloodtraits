3d30233c52eea915a9e22e791b14d562ffe1e505
caleblareau <caleblareau@g.harvard.edu>
2017-11-21 22:54:08 -0500

    plotting code

--- figures/Figure3/08_viewVariableBackground.R
+++ figures/Figure3/08_viewVariableBackground.R
@@ -1,4 +1,6 @@
 library(BuenColors)
+library(magrittr)
+library(data.table)
 
 all <- c(5,seq(10,100,5), seq(110,250,10))
 lodf <- lapply(all, function(bgn){
@@ -12,5 +14,5 @@ ggplot(lodf, aes(x = BackgroundPeaksN, y = -log10(pnorm(value,lower.tail = FALSE
   geom_line(aes(color = Var1)) + geom_point(aes(color = Var1)) +
   pretty_plot() + labs(x = "Number of ", y = "g-chromVAR Enrichment (-log10 p)", color = "") +
   facet_wrap(~Var2, scales = "free_y") +
-  theme(legend.position="bottom") +
+  theme(legend.position="bottom") + scale_color_manual(values = ejc_color_maps) +
   geom_hline(yintercept = -log10(0.05 / (16*18)), linetype = 2) 
