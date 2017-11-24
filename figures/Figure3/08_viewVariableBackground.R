e8d0fe9f767d24fb77a05341ebdcd7d5581ded2f
caleblareau <caleblareau@g.harvard.edu>
2017-11-24 13:17:15 -0500

    varying n peaks pot

--- figures/Figure3/08_viewVariableBackground.R
+++ figures/Figure3/08_viewVariableBackground.R
@@ -1,27 +1,15 @@
 library(BuenColors)
-library(magrittr)
+library(dplyr)
 library(data.table)
 
-all <- c(5,seq(10,100,5), seq(110,250,10))
-makeDF1 <- function(n){
-lodf <- lapply(all, function(bgn){
-  fn <- paste0("variableBG/variableBGpeak_", as.character(bgn), "_", "iteration_", as.character(n), ".rds")
-  df <- readRDS(fn)
-  df$BackgroundPeaksN <- bgn
-  df$iteration <- n
-  df
-}) %>% rbindlist() %>% as.data.frame()
-return(lodf)
-}
+df <- readRDS("allIterations.rds")
+df %>% group_by(Var1, Var2, npeaks) %>% summarize(zscore = mean(value)) -> meanDF
 
-alldf <- lapply(1:100, makeDF1) %>% rbindlist %>% as.data.frame
-saveRDS(alldf, file = "allIterationsAllPeaks.rds")
-
-
-
-ggplot(lodf, aes(x = BackgroundPeaksN, y = -log10(pnorm(value,lower.tail = FALSE)))) +
+p1 <- ggplot(meanDF, aes(x = npeaks, y = -log10(pnorm(zscore,lower.tail = FALSE)))) +
   geom_line(aes(color = Var1)) + geom_point(aes(color = Var1)) +
-  pretty_plot() + labs(x = "Number of ", y = "g-chromVAR Enrichment (-log10 p)", color = "") +
+  pretty_plot() + labs(x = "Number of Background Peaks", y = "g-chromVAR Enrichment (-log10 p)", color = "") +
   facet_wrap(~Var2, scales = "free_y") +
   theme(legend.position="bottom") + scale_color_manual(values = ejc_color_maps) +
   geom_hline(yintercept = -log10(0.05 / (16*18)), linetype = 2) 
+
+ggsave(p1, filename = "npeaks.pdf")
