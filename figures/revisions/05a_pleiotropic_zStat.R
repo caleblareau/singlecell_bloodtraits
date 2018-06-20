e4f3f9e949cbc211f0b7a2c1337d1acc1f0e912c
caleblareau <caleblareau@g.harvard.edu>
2018-06-20 19:34:24 -0400

    progenitor stuff

---
+++ figures/revisions/05a_pleiotropic_zStat.R
@@ -0,0 +1,22 @@
+library(gchromVAR)
+library(chromVAR)
+library(SummarizedExperiment)
+library(Matrix)
+library(BSgenome.Hsapiens.UCSC.hg19)
+
+df <- read.table("pleiotropy/all.tsv", header = TRUE)
+#df <- df[df$class == "accessible",]
+pleio_gr <- makeGRangesFromDataFrame(df)
+
+# Import counts and normalize
+counts <-  data.matrix(fread("../../data/bulk/ATAC/29August2017_EJCsamples_allReads_500bp.counts.txt")) 
+ATAC.cpm <- round(sweep(counts, 2, colSums(counts), FUN="/") * 1000000, 1)
+ATAC.cpm.log2 <- log2(ATAC.cpm+1)
+
+peaksdf <- fread("../../data/bulk/ATAC/29August2017_EJCsamples_allReads_500bp.bed")
+peaks <- makeGRangesFromDataFrame(peaksdf, seqnames = "V1", start.field = "V2", end.field = "V3")
+
+progenitors <- as.vector(ATAC.cpm[queryHits(ov),c("HSC", "CLP", "CMP", "GMP-A", "GMP-B", "GMP-C", "LMPP", "MEP", "MPP")])
+committed <- as.vector(ATAC.cpm[queryHits(ov),c("B", "CD4", "CD8", "Ery", "mDC", "Mega", "Mono", "NK", "pDC")])
+
+t.test(progenitors, committed)
