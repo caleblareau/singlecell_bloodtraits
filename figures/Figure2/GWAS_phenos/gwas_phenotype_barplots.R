a5f4ba625848f78b1833529dc311a0d77c42fbae
erikbao <ebao@gold.broadinstitute.org>
2018-07-13 15:21:03 -0400

    reordered bars

--- figures/Figure2/GWAS_phenos/gwas_phenotype_barplots.R
+++ figures/Figure2/GWAS_phenos/gwas_phenotype_barplots.R
@@ -125,15 +125,15 @@ eval_gene <- function(rs2, rs2_l, rs1, rs1_l,trait="EO_COUNT"){
   data.frame(name = paste0("rs8: ", rs2_l, "\nrs445: ", rs1_l), estimate = mean(v[,trait]), se = sd(v[,trait])/sqrt(length(v[,trait])))
 }
 CDK6plotdf <- rbind(
-  eval_gene(rs2 = 2, rs2_l = "TT", rs1 = 2, rs1_l = "TT"),
-  eval_gene(rs2 = 1, rs2_l = "CT", rs1 = 2, rs1_l = "TT"),
-  eval_gene(rs2 = 0, rs2_l = "CC", rs1 = 2, rs1_l = "TT"),
+  eval_gene(rs2 = 2, rs2_l = "TT", rs1 = 0, rs1_l = "CC"),
+  eval_gene(rs2 = 1, rs2_l = "CT", rs1 = 0, rs1_l = "CC"),
+  eval_gene(rs2 = 0, rs2_l = "CC", rs1 = 0, rs1_l = "CC"),
   eval_gene(rs2 = 2, rs2_l = "TT", rs1 = 1, rs1_l = "CT"),
   eval_gene(rs2 = 1, rs2_l = "CT", rs1 = 1, rs1_l = "CT"),
   eval_gene(rs2 = 0, rs2_l = "CC", rs1 = 1, rs1_l = "CT"),
-  eval_gene(rs2 = 2, rs2_l = "TT", rs1 = 0, rs1_l = "CC"),
-  eval_gene(rs2 = 1, rs2_l = "CT", rs1 = 0, rs1_l = "CC"),
-  eval_gene(rs2 = 0, rs2_l = "CC", rs1 = 0, rs1_l = "CC")
+  eval_gene(rs2 = 2, rs2_l = "TT", rs1 = 2, rs1_l = "TT"),
+  eval_gene(rs2 = 1, rs2_l = "CT", rs1 = 2, rs1_l = "TT"),
+  eval_gene(rs2 = 0, rs2_l = "CC", rs1 = 2, rs1_l = "TT")
 )
 
 CDK6plotdf <- CDK6plotdf[complete.cases(CDK6plotdf),]
